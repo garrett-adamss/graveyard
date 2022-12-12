ddde7cfa2a8c891cb5f652ff4802cbb7eb2f96ac
Garrett Adams <garrettadams27@gmail.com>
2022-12-12 00:20:45 -0700

    Revert "some styles"
    This reverts commit b293fb280dd8de1635c504aec98c1da2969e49bc.

--- graveyard.client/src/components/ItemCard.vue
+++ graveyard.client/src/components/ItemCard.vue
@@ -1,10 +1,8 @@
 <template>
-    <div class="col-md-3 item-card" @click="toItemsDetailPage()">
+    <div class="col-3 item-card">
         <img src="https://thiscatdoesnotexist.com/"/>
-        <div class="text-box">
-            <p class="name">Cat</p>
-            <p class="price">$150</p>
-        </div>
+        <p class="name">Cat</p>
+        <p class="price">$150</p>
     </div>
 
 
@@ -14,17 +12,8 @@
 
 export default {
    setup(){
-    // toItemsDetailPage(){
-    //     try {
-           
-    //     }
-    //     catch (error) {
-    //        logger.error(error)
-    //        Pop.toast(error.message, 'error')
-    //     }
-    // }
       return {
-        
+ 
       }
    }
 }
@@ -35,23 +24,14 @@ img{
     width: -webkit-fill-available;
 }
 .item-card{
-    position: relative;
     background-color: rgb(205, 205, 205);
-    border-style: solid;
-    border-color: red;
-}
-.text-box{
-    position: absolute;
+    
 }
 .name{
-    position: absolute;
-    bottom: 0;
     font-size: large;
     font-weight: 400;
 }
 .price{
-    position: absolute;
-    bottom: 0;
     font-size: medium;
     font-weight: 500;
 }
---
+++ graveyard.client/src/components/ShopItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-3">
+        <img src="https://thiscatdoesnotexist.com/"/>
+        <h1>Cat</h1>
+        <p>$150</p>
+    </div>
+</template>
+ 
+<script>
+export default {
+   setup(){
+      return {
+ 
+      }
+   }
+}
+</script>
+ 
+<style> 
+img{
+    width: -webkit-fill-available;
+}
+</style>
