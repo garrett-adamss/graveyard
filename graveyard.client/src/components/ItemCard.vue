e842129ee49aaf095cd06d79dd02f31e1c8bfc8a
Garrett <garrettadams27@gmail.com>
2022-12-13 21:04:30 -0700

    shop page looking okay

--- graveyard.client/src/components/ItemCard.vue
+++ graveyard.client/src/components/ItemCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-3 item-card">
         <img class="item-img" src="https://thiscatdoesnotexist.com/"/>
-        <p class="name">Cat</p>
+        <p class="name">Cat T-shirt</p>
         <p class="price">$150</p>
     </div>
 
@@ -24,17 +24,26 @@ export default {
     width: -webkit-fill-available;
 }
 .item-card{
+    position: relative;
+    margin-bottom: 10vh;
     /* background-color: rgb(205, 205, 205); */
-    border-style: solid;
-    border-color: red;
-    
+    /* border-style:  solid; */
+    /* border-color: red; */   
+    transition: transform 250ms;
+}
+.item-card:hover{
+    transform: translateY(-4px);
+    cursor: pointer;
 }
 .name{
+    position: absolute;
     font-size: large;
     font-weight: 400;
 }
 .price{
-    font-size: medium;
+    position: absolute;
+    font-size: 21px;
     font-weight: 500;
+    bottom: -12vh;
 }
 </style>
--- graveyard.client/src/pages/ShopPage.vue
+++ graveyard.client/src/pages/ShopPage.vue
@@ -6,8 +6,22 @@
         <div class="input-group rounded">
           <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
           <span class="input-group-text border-0" id="search-addon">
-          <i class="mdi mdi-search"></i>
+          <i class="mdi mdi-magnify"></i>
           </span>
+        </div>
+        <div class="filter">
+          <div>
+            All
+          </div>
+          <div>
+            T-shirts
+          </div>
+        </div>
+        <div class="contact">
+          <i class="mdi mdi-contact"></i>
+        </div>
+        <div>
+
         </div>
       </div>
       <div class="col-10">
