db4151033d4c49fcb4dde0c478c56d22d61ba3d2
Garrett Adams <garrettadams27@gmail.com>
2022-12-13 19:01:03 -0700

    fixed the home screen

--- graveyard.client/src/components/ItemCard.vue
+++ graveyard.client/src/components/ItemCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="col-3 item-card">
-        <img src="https://thiscatdoesnotexist.com/"/>
+        <img class="item-img" src="https://thiscatdoesnotexist.com/"/>
         <p class="name">Cat</p>
         <p class="price">$150</p>
     </div>
@@ -20,7 +20,7 @@ export default {
 </script>
  
 <style> 
-img{
+.item-img{
     width: -webkit-fill-available;
 }
 .item-card{
